faa998974e5c86a8867e9803e9fe3000cea1b67d
nurulamin092 <mnurulamin092@gmial.com>
2022-03-05 14:42:07 +0600

    input id ok

--- js/shoping.js
+++ js/shoping.js
@@ -1,1 +1,7 @@
-alert('Bismillah')
+
+const addItem = () => {
+    const nameField = document.getElementById('product-name')
+    const name = nameField.value;
+    console.log(name);
+    nameField.value = '';
+}
