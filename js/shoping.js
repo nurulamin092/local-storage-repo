f1f9db3dff04a0b2949515696fd668fff646d1db
nurulamin092 <mnurulamin092@gmial.com>
2022-03-05 15:12:58 +0600

    product show display

--- js/shoping.js
+++ js/shoping.js
@@ -2,6 +2,15 @@
 const addItem = () => {
     const nameField = document.getElementById('product-name')
     const name = nameField.value;
+    displayProduct(name)
     console.log(name);
     nameField.value = '';
+}
+
+const displayProduct = name => {
+    const product = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerText = name;
+    product.appendChild(li)
+
 }
